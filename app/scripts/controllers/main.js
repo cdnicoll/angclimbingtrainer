80d15a481ab539bd45189791193b1cfad77a73ec
Cody Nicoll <conicoll@ea.com>
2014-10-22 15:17:59 -0700

    Application cleanup and setup

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,50 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name angBoulderTrainerApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the angBoulderTrainerApp
- */
-angular.module('angBoulderTrainerApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+app.controller('MainCtrl', function () {
+     
+    var pullUpModel = {
+        'name':'Pull Up',
+        'leftHand': 1,
+        'rightHand': 1,
+        'reps':3,
+        'break':3
+    };
+
+     var exercises = {
+        'pullUp': pullUpModel,
+        'kneeRaises':'Knee Raises',
+        'deadHang':'Dead Hang',
+        'bentArmHang': 'Bent Arm Hang',
+        'lHang': 'L Hang',
+        'frontLeaver': 'Front Leaver',
+        'pushUps': 'Push Ups',
+        'sitUps': 'Sit Ups',
+        'plank': {
+            'side':'Side',
+            'front':'Front'
+        }
+     };
+
+     // one arm pullups deadhangs right: (n) left: 0
+
+     console.log(exercises);
+
+     var workout1 = [
+        {
+            'exercise':exercises.pullUp,
+            'leftHand':1,
+            'rightHand':1,
+            'reps':3,
+            'break':3
+        },
+        {
+            'exercise':exercises.pullUp,
+            'leftHand':1,
+            'rightHand':1,
+            'reps':6,
+            'break':3
+        }
     ];
-  });
+
+     console.log(workout1);
+});
