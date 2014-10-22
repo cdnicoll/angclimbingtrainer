b2e528216e1bd128ed3e02edb6b81a692c3dca6a
Cody Nicoll <conicoll@ea.com>
2014-10-22 12:18:49 -0700

    init commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angBoulderTrainerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angBoulderTrainerApp
+ */
+angular.module('angBoulderTrainerApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angBoulderTrainerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angBoulderTrainerApp
+ */
+angular.module('angBoulderTrainerApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
